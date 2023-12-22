cb99adc4fe21d3690f37559258bcdb5b465278de
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2023-12-22 15:05:49 -0600

    Update prisma.ts

--- src/prisma.ts
+++ src/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient({
     datasources: {
         db: {
-            url: process.env["LOG_DATABASE_URL"],
+            url: process?.env?.["LOG_DATABASE_URL"],
         },
     },
 })
