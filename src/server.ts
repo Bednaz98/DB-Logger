0399b9680c501c69656fe2aea07b7f9ec25850e9
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2023-12-23 13:18:44 -0600

    add logging for db url string

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,17 @@ app.use(cors());
 app.disable('x-powered-by');
 app.use('/db', LoggerRoute);
 
+function getDBStringDebug() {
+    const dbString = process.env?.["LOG_DATABASE_URL"]
+    if (!dbString) return "N/A";
+    const firstSplit = dbString.split('://')
+    const dbType = firstSplit[0]
+    const ipAddress = firstSplit[1].split(':')[1].split("@")[1]
+    const port = firstSplit[1].split(':')[2].split("/")[0]
+
+    return (`\n- db type: ${dbType}\n- host ip: ${ipAddress}:${port}`);
+}
+
 function getPort() {
     const loggerPort = Number(process?.env["LOGGER_PORT"])
     if (!isNaN(loggerPort)) {
@@ -30,5 +41,7 @@ function getPort() {
     console.log("using default port: ", 3000);
     return 3000;
 }
+
 const port = getPort();
+console.log(getDBStringDebug());
 app.listen(port, () => { console.log(`Logger Server Start on Port ${port}`) });
