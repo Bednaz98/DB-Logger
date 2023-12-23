66990b176efae3323f1207266d56aec402cceba1
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2023-12-23 10:39:27 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { LoggerRoute } from "./expressRoute";
 
 
 
-
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -17,12 +17,18 @@ app.disable('x-powered-by');
 app.use('/db', LoggerRoute);
 
 function getPort() {
-    const loggerPort = process?.env["LOGGER_PORT"]
-    if (loggerPort) return Number(loggerPort)
-    const serverPort = process.env?.["SERVER_PORT"]
-    if (serverPort) return Number(serverPort)
-    return 3000
+    const loggerPort = Number(process?.env["LOGGER_PORT"])
+    if (!isNaN(loggerPort)) {
+        console.log("detected logger port: ", loggerPort)
+        return loggerPort;
+    }
+    const serverPort = Number(process.env?.["SERVER_PORT"])
+    if (!isNaN(serverPort)) {
+        console.log("detected sever port: ", serverPort)
+        return Number(serverPort)
+    }
+    console.log("using default port: ", 3000);
+    return 3000;
 }
 const port = getPort();
-
 app.listen(port, () => { console.log(`Logger Server Start on Port ${port}`) });
